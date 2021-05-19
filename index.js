617b9de54bf812c9b1d594af61fd68ea4b277915
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-19 02:08:46 -0400

    commit tree 6e433918537ddd80fdc226dee929feb4fc08c06e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(names, match) {
+    return names.filter((names) => names.toLowerCase() == match.toLowerCase());
+}
+
+function fuzzyMatch(names, match) {
+    for (let i of match) {
+        return names.filter((names) => names[0].toLowerCase() == i.toLowerCase())
+    }
+}
+
+function matchName(names, match) {
+    return names.filter((names) => names.name == match)
+}
